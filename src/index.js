dfc431a9f651b89b173405b2c4e0bfc82f7ffe83
Luisito Canlas <luisitocanlas@gmail.com>
2024-06-07 13:36:43 -0700

    Setup webpack dependencies and file structure

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './styles/style.css';
+
+if (process.env.NODE_ENV !== 'production') {
+	console.log('Looks like we are in development mode!');
+} else {
+	console.log('Looks like we are in production mode!');
+}
