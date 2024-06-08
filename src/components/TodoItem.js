d449aba8e792206857bd2af8c3f407199e6a52da
Luisito Canlas <luisitocanlas@gmail.com>
2024-06-07 18:50:50 -0700

    Updated dueDate value

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,8 +2,8 @@ export default class TodoItem {
 	constructor(title, description, dueDate, priority) {
 		this.title = title;
 		this.description = description;
+		this.dueDate = dueDate;
 		this.priority = priority;
-		this.dueDate = null; // Default value for dueDate
 		this.isCompleted = false; // Default value for isCompleted
 	}
 }
